5b29e25e51daa5be69512a8125e24054797bc79f
Daniel August Jensen <daj@live.no>
2019-02-10 14:28:42 +0100

    Get features and rebuild playlist object

--- playlist.py
+++ playlist.py
@@ -3,20 +3,34 @@ import sys
 import pprint
 from spotipy.oauth2 import SpotifyClientCredentials
 
+# Log in
 client_credentials_manager = SpotifyClientCredentials(client_id='43a1cf4b58e24cc9b5fb5a90ad8c1bb7',
                                                       client_secret='4436a0ee76a3474092a0dfe60703c2dc')
 spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-# results = spotify.user_playlist("dadsh", "spotify:user:dadsh:playlist:3TAWHF1eC98T4IT0anOIRW", fields="tracks")
-
+# Get playlist
 user = "dadsh"
-playlist = "spotify:user:dadsh:playlist:3TAWHF1eC98T4IT0anOIRW"
-
+playlist = "spotify:user:dadsh:playlist:5tecqNtnQV175T2pSbyoVe"
 results = spotify.user_playlist_tracks(user, playlist, fields="items(track(artists(name),name,popularity,uri))")
+# pprint.pprint(results)
 
-features = spotify.audio_features(["spotify:track:62ubbkWAhTOftwgRjHS1UH"])
+# Get URIs of tracks
+uris = []
+for i in range(len(results['items'])):
+    uri = results['items'][i]['track']['uri']
+    uris.append(uri)
 
+# Get features of tracks
+features = spotify.audio_features(uris)
+# pprint.pprint(features)
 
-pprint.pprint(results['items'][3]['track']['name'])
-pprint.pprint(results['items'][3]['track']['uri'])
+# Rebuild playlist object
+pl = results['items']
+for i in range(len(pl)):
+    pl[i]['track']['features'] = features[i]
+print(type(pl))
+pprint.pprint(pl)
+
+# pprint.pprint(results['items'][3]['track']['name'])
+# pprint.pprint(results['items'][3]['track']['uri'])
 # pprint.pprint(features)
